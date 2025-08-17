42db2a0f52bac123a1de36c14ed819461b179ade
barath1248 <barathrv124@gmail.com>
2025-08-17 17:24:20 +0530

    created navbar

---
+++ Frontend/vite-project/src/Components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../Styles/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+
+      <div className="brand">
+        <h1 className="brand-title">Quizoo</h1>
+      </div>
+      
+   
+      <div className="nav-items">
+        <span className="nav-text">Quiz Generator</span>
+        <a 
+          href="https://github.com/barath1248" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="nav-link"
+        >
+          GitHub
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
